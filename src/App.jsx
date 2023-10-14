1202f8943ebbcf007bfa07bea0ac763a5c7b1d33
Konaschkov <konashkov23@gmail.com>
2023-10-14 14:52:21 +0200

    Add API

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
         <Header />
         <main className="container content">
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/">
+              <Home />
+            </Route>
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={NotFound} />
